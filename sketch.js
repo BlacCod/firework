b2cbc55cd2f44196663cfe88747f370761b55304
BlacCod <vuhuynguyen4@gmail.com>
2018-12-31 23:55:10 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ var gravity;
 function setup() {
   createCanvas(1024, 768)
   colorMode(HSB);
-  gravity = createVector(0, 0.09);
+  gravity = createVector(0, 0.12);
   stroke(255);
   strokeWeight(4);
   background(0);
